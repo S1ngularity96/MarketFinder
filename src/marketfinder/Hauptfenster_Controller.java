136b48f7d4f8ab1a6f69df2f571384dfc1050392
Andrei Alexeev <andrei.alexeev@alumni.fh-aachen.de>
2018-02-24 12:39:49 +0100

    Controller für FXML

---
+++ src/marketfinder/Hauptfenster_Controller.java
@@ -0,0 +1,32 @@
+
+package marketfinder;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Andrei
+ */
+public class Hauptfenster_Controller implements Initializable{
+    
+    
+    
+    
+    
+    
+    
+    
+    /**
+     * Starteigenschaften werden hier an die Benutzer- 
+     * oberfläche übergeben
+     * @param location
+     * @param resources 
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
