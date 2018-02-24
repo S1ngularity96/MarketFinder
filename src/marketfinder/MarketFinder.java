f8a830d9204b1c949e51c2bed55aee05a854f8bf
Andrei Alexeev <31422446+S1ngularity9696@users.noreply.github.com>
2018-02-24 01:52:22 +0100

    Dynamic GUI added

--- src/marketfinder/MarketFinder.java
+++ src/marketfinder/MarketFinder.java
@@ -5,6 +5,14 @@
  */
 package marketfinder;
 
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import jdk.nashorn.internal.codegen.CompilerConstants;
+
 
 
 
@@ -12,13 +20,58 @@ package marketfinder;
  *
  * @author Andrei
  */
-public class MarketFinder {
+public class MarketFinder extends Application {
 
+    private String mainWindow = "Hauptfenster.fxml";
+    
+    private Stage primaryStage;
+    
+    
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        ladeFenster(mainWindow);
+        primaryStage.show();
+        
+    }
+    
+    /**
+    * Lädt ein neues Scene für die Stage
+    * @param myWindow 
+    */
+    private void ladeFenster(String myWindow){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(myWindow));
+        
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException ex) {
+            System.err.println("MainApplication kann die FXML- Datei nicht laden.");
+            ex.printStackTrace();
+            return;
+        }
+        
+        
+        
+        
+        
+        
+        
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("MarketFinder");
+        primaryStage.setMinWidth(1070);
+        primaryStage.setMinHeight(760);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.centerOnScreen();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Hier kommt die Main
+        launch(args);
     }
     
     
